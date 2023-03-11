8a7a495552b28e979f7834e9f8f811c166db2981
luhvpreet <luhvpreet@gmail.com>
2023-03-11 13:26:20 -0800

    added signup and login forms

---
+++ app/src/main/java/com/example/mlej/SignupAccountTypeActivity.java
@@ -0,0 +1,14 @@
+package com.example.mlej;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupAccountTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_account_type);
+    }
+}

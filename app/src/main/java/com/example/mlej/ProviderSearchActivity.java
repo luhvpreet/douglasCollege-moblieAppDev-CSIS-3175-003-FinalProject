f066763d23e28037378ff013a27bd6fc06319ae4
Jichi Zhang <i@jichi.ca>
2023-03-22 21:38:50 -0700

    Fixed issue where wrong profile is displayed when search result has been filtered

--- app/src/main/java/com/example/mlej/ProviderSearchActivity.java
+++ app/src/main/java/com/example/mlej/ProviderSearchActivity.java
@@ -8,10 +8,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ProviderSearchActivity extends AppCompatActivity {
     DatabaseHelper db;
+    ArrayAdapter<String> adapter;
+    List<ProviderItemModel> providers;
+    List<ProviderItemModel> filteredProviders;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,26 +27,43 @@ public class ProviderSearchActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.listView);
 
         // get the list of providers using getProviders()
-        List providers = db.getProviders();
+        providers = db.getProviders();
+        filteredProviders = new ArrayList<>(providers);
         // get the name of each provider and add it to an array
         String[] providerNames = new String[providers.size()];
         for (int i = 0; i < providers.size(); i++) {
             // get the name of the provider using getProviderName() of the provider item model
             providerNames[i] = ((ProviderItemModel) providers.get(i)).getProviderName();
         }
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, providerNames);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, providerNames);
         listView.setAdapter(adapter);
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                adapter.getFilter().filter(query);
+                // filter the copy of the original list, not the original list itself
+                filteredProviders = filterList(providers, query);
+                String[] filteredProviderNames = new String[filteredProviders.size()];
+                for (int i = 0; i < filteredProviders.size(); i++) {
+                    filteredProviderNames[i] = filteredProviders.get(i).getProviderName();
+                }
+                ArrayList<String> filteredList = new ArrayList<>(Arrays.asList(filteredProviderNames));
+                adapter = new ArrayAdapter<>(ProviderSearchActivity.this, android.R.layout.simple_list_item_1, filteredList);
+                listView.setAdapter(adapter);
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                adapter.getFilter().filter(newText);
+                // filter the copy of the original list, not the original list itself
+                filteredProviders = filterList(providers, newText);
+                String[] filteredProviderNames = new String[filteredProviders.size()];
+                for (int i = 0; i < filteredProviders.size(); i++) {
+                    filteredProviderNames[i] = filteredProviders.get(i).getProviderName();
+                }
+                ArrayList<String> filteredList = new ArrayList<>(Arrays.asList(filteredProviderNames));
+                adapter = new ArrayAdapter<>(ProviderSearchActivity.this, android.R.layout.simple_list_item_1, filteredList);
+                listView.setAdapter(adapter);
                 return false;
             }
         });
@@ -49,7 +71,7 @@ public class ProviderSearchActivity extends AppCompatActivity {
         // when a provider is clicked, go to the provider profile page
         listView.setOnItemClickListener((parent, view, position, id) -> {
             // get the user id of the provider that was clicked using its item id
-            int providerId = ((ProviderItemModel) providers.get((int) id)).getProviderId();
+            int providerId = filteredProviders.get(position).getProviderId();
             // go to the provider profile page
             // pass the provider id to the provider profile page
             Intent intent = new Intent(ProviderSearchActivity.this, ProfileViewActivity.class);
@@ -57,5 +79,14 @@ public class ProviderSearchActivity extends AppCompatActivity {
             intent.putExtra("name", db.getCompanyName(providerId));
             startActivity(intent);
         });
+    }
+    private List<ProviderItemModel> filterList(List<ProviderItemModel> originalList, String query) {
+        List<ProviderItemModel> filteredList = new ArrayList<>();
+        for (ProviderItemModel provider : originalList) {
+            if (provider.getProviderName().toLowerCase().contains(query.toLowerCase())) {
+                filteredList.add(provider);
+            }
+        }
+        return filteredList;
     }
 }

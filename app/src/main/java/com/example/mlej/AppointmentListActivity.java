c9d4b9475ce5e192fafc7ae7ec6b71af93a8ed1d
Jichi Zhang <i@jichi.ca>
2023-03-24 13:51:05 -0700

    Removed appointment list title

--- app/src/main/java/com/example/mlej/AppointmentListActivity.java
+++ app/src/main/java/com/example/mlej/AppointmentListActivity.java
@@ -32,6 +32,8 @@ public class AppointmentListActivity extends AppCompatActivity{
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
         userId = preferences.getInt("USERID",0);
 
+        setTitle("Upcoming Appointments");
+
         initData(userId);
 
         System.out.println("AppointmentListActivity: "+userId);

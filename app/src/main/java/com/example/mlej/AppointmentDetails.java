df6cdf59eb0dcfaf9884573611a4f5ef7c5bcb93
Jichi Zhang <i@jichi.ca>
2023-03-29 22:04:38 -0700

    Innitial implementation of report

--- app/src/main/java/com/example/mlej/AppointmentDetails.java
+++ app/src/main/java/com/example/mlej/AppointmentDetails.java
@@ -30,6 +30,7 @@ public class AppointmentDetails extends AppCompatActivity {
         Button btnEditCustomerProfile = findViewById(R.id.btnEditCustomerProfile);
         Button btnCancelAppointment = findViewById(R.id.btnCancelAppointment);
         Button btnEditAppointment = findViewById(R.id.btnEditAppointment);
+        Button btnGenerateReport = findViewById(R.id.btnGenerateReport);
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         if (intent != null){
@@ -49,11 +50,13 @@ public class AppointmentDetails extends AppCompatActivity {
                 btnRemindCustomer.setVisibility(View.INVISIBLE);
                 btnCancelAppointment.setVisibility(View.INVISIBLE);
                 btnEditAppointment.setVisibility(View.INVISIBLE);
+                btnGenerateReport.setVisibility(View.VISIBLE);
             }
 
             if (userId == customerId) {
                 btnRemindCustomer.setVisibility(View.INVISIBLE);
                 btnEditCustomerProfile.setVisibility(View.INVISIBLE);
+                btnGenerateReport.setVisibility(View.INVISIBLE);
             }
 
             AppointmentItemModel aim = db.getAppointment(appointmentId);
@@ -114,6 +117,15 @@ public class AppointmentDetails extends AppCompatActivity {
                     else {
                         Toast.makeText(AppointmentDetails.this, "Appointment cancellation failed!", Toast.LENGTH_SHORT).show();
                     }
+                }
+            });
+
+            btnGenerateReport.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(AppointmentDetails.this, ReportActivity.class);
+                    intent.putExtra("appointmentId", appointmentId);
+                    startActivity(intent);
                 }
             });
 
